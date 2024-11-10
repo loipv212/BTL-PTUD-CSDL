96094592f42ec1e8657a9caed189731213f3b6c4
Linh0601 <dothuylinh06012004@gmail.com>
2024-11-10 13:39:55 +0700

    Add files via upload

---
+++ QLKTX/Quanlytiennuoc/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Quanlytiennuoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox3 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox3);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1088, 202);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(12, 231);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1088, 282);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Danh sách";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(392, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(157, 34);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Phòng";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(286, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Chỉ số cũ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(569, 44);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Chỉ số mới";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 28);
+            label4.TabIndex = 4;
+            label4.Text = "Từ ngày";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(286, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 28);
+            label5.TabIndex = 5;
+            label5.Text = "Đến ngày";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(570, 91);
+            label6.Name = "label6";
+            label6.Size = new Size(103, 28);
+            label6.TabIndex = 6;
+            label6.Text = "Thành tiền";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(679, 41);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(157, 34);
+            textBox2.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(104, 87);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(157, 34);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(392, 86);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(157, 34);
+            dateTimePicker2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(679, 87);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(157, 34);
+            textBox3.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(104, 41);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(157, 36);
+            comboBox1.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 33);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1053, 243);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(16, 141);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 37);
+            button1.TabIndex = 12;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(149, 141);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 37);
+            button2.TabIndex = 13;
+            button2.Text = "Sửa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(278, 141);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 37);
+            button3.TabIndex = 14;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(radioButton1);
+            groupBox3.Controls.Add(radioButton3);
+            groupBox3.Location = new Point(865, 44);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(204, 119);
+            groupBox3.TabIndex = 17;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Thanh toán";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(21, 33);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(157, 32);
+            radioButton3.TabIndex = 16;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Đã thanh toán";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(21, 71);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(178, 32);
+            radioButton1.TabIndex = 17;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Chưa thanh toán";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1107, 630);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "QUẢN LÝ TIỀN NƯỚC";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private TextBox textBox3;
+        private DateTimePicker dateTimePicker2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox3;
+        private RadioButton radioButton3;
+        private RadioButton radioButton1;
+    }
+}
